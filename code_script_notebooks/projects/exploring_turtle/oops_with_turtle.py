dcb15b5b220fba34d1f845216a09445ecb3b493f
insightbuilder <kamaljp@gmail.com>
2024-01-01 15:05:57 +0530

    progressed data manipulation torch

---
+++ code_script_notebooks/projects/exploring_turtle/oops_with_turtle.py
@@ -0,0 +1,27 @@
+# Script explores using Turtle along with OOP
+
+from turtle import Turtle, mainloop 
+
+
+if __name__ == '__main__':
+    a = Turtle(shape='classic')
+    b = Turtle(shape='square')
+
+    a.shapesize(1, 2)
+    b.shapesize(1, 1, 1)
+
+    a.penup()
+    b.penup()
+
+    a.goto(100, 100)
+    b.goto(100, -100)
+
+    a.pendown()
+    b.pendown()
+
+    a.circle(50)
+    b.circle(200)
+
+    a.write("This is turtle 1")
+    b.write("This is turtle b")
+    mainloop()
