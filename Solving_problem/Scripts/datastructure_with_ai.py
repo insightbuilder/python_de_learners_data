b302d7747cb59a40fd4d617458ce26f30041c068
insightbuilder <kamaljp@gmail.com>
2024-01-19 07:00:59 +0530

    updating file resolution

--- Solving_problem/Scripts/datastructure_with_ai.py
+++ Solving_problem/Scripts/datastructure_with_ai.py
@@ -2,9 +2,10 @@
 # their salient features, problems each can solve,
 # their own implementation, and one of the problem
 # solution
-
+import pathlib
 import streamlit as st
 import json
+st.set_page_config(layout='wide')
 # provide the title
 st.title("Learn & Document 27 Data structures with GPT 3.5")
 # declare the list of data structures
@@ -16,19 +17,23 @@ ds_list = ["List", "Dict", "Tuples", "Strings", "Deque", "NamedTuple",
            "PriorityQueues", "DisjointSet", "BinaryIndexedTree",
            "SuffixArray"]
 
+# locating the file
+code_dir = pathlib.Path(__file__).parent.resolve()
+files_location = code_dir / "complete_ds_data.json"
+files_location = files_location.resolve()
+st.write(files_location)
 # reading the complete_ds_data.json file
-with open('complete_ds_data.json', 'r') as cds:
+with open(files_location, 'r') as cds:
     final_json = json.load(cds)
 
 select_ds = st.radio('Select a Datastructure', ds_list)
 
 selected_data = final_json[select_ds]
-
 st.title(f"{select_ds} Introduction")
 st.write(selected_data['salient_points'])
 st.title(f"{select_ds} Applied Problem")
 st.write(selected_data['problem_solved'])
 st.title(f"{select_ds} Python Implementation")
-st.write(selected_data['python_implementation'])
+st.write(f"```python{selected_data['python_implementation']}```")
 st.title(f"{select_ds} Solution For Example Problem")
-st.write(selected_data['solution_implement'])
+st.write(f"```python{selected_data['solution_implement']}```")
