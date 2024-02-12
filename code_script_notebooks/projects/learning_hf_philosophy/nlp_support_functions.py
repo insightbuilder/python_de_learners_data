20c7b6c80b9c2493b85b94fe4f010a0932b18946
kamalabot <kamaljp@gmail.com>
2024-02-12 16:29:44 +0530

    nlp updated

--- code_script_notebooks/projects/learning_hf_philosophy/nlp_support_functions.py
+++ code_script_notebooks/projects/learning_hf_philosophy/nlp_support_functions.py
@@ -203,6 +203,7 @@ def preprocess_function(examples: List, tokenizer, tokenized_examples):
 
 
 def compute_metrics(eval_predictions):
+    """accuracy metrics"""
     predictions, label_ids = eval_predictions
     preds = np.argmax(predictions, axis=1)
     return {"accuracy": (preds == label_ids).astype(np.float32).mean().item()}
